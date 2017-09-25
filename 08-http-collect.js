25c442686ab790494bb3b7b82b8e7ca2d2cc6b93
neko2016 <honglok16@hotmail.com>
2017-09-25 21:38:01 +0100

    exercise 08 http server response

---
+++ 08-http-collect.js
@@ -0,0 +1,14 @@
+var http = require('http');
+var url = process.argv[2];
+
+http.get(url, function(response){
+    var output = "";
+    response.setEncoding('utf8');
+    response.on('data', function(data){
+        output += data
+    })
+    response.on('end', function(){
+        console.log(output.length);
+        console.log(output);
+    })
+})
